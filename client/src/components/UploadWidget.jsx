ba355f48f30a48a70c38901d0ad69bcc759998d5
sanket bodke <sanketbodke26@gmail.com>
2023-12-31 17:57:25 +0530

    upload widget component add

---
+++ client/src/components/UploadWidget.jsx
@@ -0,0 +1,31 @@
+// UploadWidget.jsx
+import { useEffect, useRef } from 'react';
+
+const UploadWidget = ({ onImageUpload }) => {
+    const cloudinaryRef = useRef();
+    const widgetRef = useRef();
+
+    useEffect(() => {
+        cloudinaryRef.current = window.cloudinary;
+        widgetRef.current = cloudinaryRef.current.createUploadWidget({
+            cloudName: 'sanket12',
+            upload_preset: 'plcy9ur1',
+        }, function (error, result) {
+            if (!error && result && result.event === "success") {
+                // Call the callback function with the uploaded image URL
+                onImageUpload(result.info.secure_url);
+            } else {
+                console.error("Upload error:", error);
+            }
+        });
+
+    }, [onImageUpload]);
+
+    return (
+        <>
+            <button onClick={() => widgetRef.current.open()}>upload</button>
+        </>
+    );
+};
+
+export default UploadWidget;
